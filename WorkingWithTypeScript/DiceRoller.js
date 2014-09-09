81bea8a9d9af045f99137714fd5f0b7622e4ac18
Bill Wagner <bill.w.wagner@outlook.com>
2014-09-09 16:51:48 -0400

    Create the DiceRoller class
    This includes a reference node for the IRoll interface because that
    interface will define the behavior of the elements.

---
+++ WorkingWithTypeScript/DiceRoller.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="iroll.ts" />
+var DiceRoller = (function () {
+    function DiceRoller() {
+    }
+    return DiceRoller;
+})();
+//# sourceMappingURL=DiceRoller.js.map
---
+++ WorkingWithTypeScript/DiceRoller.ts
@@ -0,0 +1,3 @@
+﻿/// <reference path="iroll.ts" />
+class DiceRoller {
+} 
